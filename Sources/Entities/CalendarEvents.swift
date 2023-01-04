f3b3a80518cc2e0951708e1d134a3915289d7493
alexdremov <dremov.me@gmail.com>
2023-01-04 17:25:56 +0300

    Podfile removed in favor of SPM

---
+++ Sources/Entities/CalendarEvents.swift
@@ -0,0 +1,8 @@
+////
+// Created for  in 2022
+// Using Swift 5.0
+//  Created on 02.05.2022 by Alex Dremov
+// 
+//
+
+import Foundation
---
+++ Sources/Entities/SummaryProfile.swift
@@ -0,0 +1,8 @@
+//
+// Created in 2022
+// Using Swift 5.0
+// On 02.05.2022 by Alex Dremov
+// 
+//
+
+import Foundation
